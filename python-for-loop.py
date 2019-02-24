a93fce6ae939078d26eff069066aa186e14d5297
Aizaz Bokhari <abokhari@icstars.org>
2019-02-24 02:02:03 -0600

    updated for loop

--- python-for-loop.py
+++ python-for-loop.py
@@ -1,7 +1,11 @@
 x = 0
 for aliia in range(0,101):
-    if (aliia%3==0):
+    if (aliia%3==0 and aliia%5==0):
+        print("fizzbuzz")
+    elif (aliia%3==0):
         print("fizz")
+    elif (aliia%5==0):
+        print("buzz")
     else:
         print (aliia)
         
